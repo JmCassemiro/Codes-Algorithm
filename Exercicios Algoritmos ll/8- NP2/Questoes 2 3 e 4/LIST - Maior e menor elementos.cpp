7ae5576e67decc521bf14809931912e6c91751d6
João Marcos Cassemiro <joaomarcos.jm@ges.inatel.br>
2025-02-05 15:00:02 -0300

    Editando arquivos

--- Exercicios Algoritmos ll/8- NP2/Questoes 2 3 e 4/LIST - Maior e menor elementos.cpp
+++ Exercicios Algoritmos ll/8- NP2/Questoes 2 3 e 4/LIST - Maior e menor elementos.cpp
@@ -3,52 +3,50 @@
 
 using namespace std;
 
-
 void maiorMenor(list<int> lista, int &menor, int &maior)
 {
-	list<int>::iterator p; //ponteiro pra andar na lista
-	
+	list<int>::iterator p; // ponteiro pra andar na lista
+
 	p = lista.begin();
 	menor = *p;
 	maior = *p;
-	
+
 	while (p != lista.end())
 	{
 		if (*p > maior)
-		    maior = *p;
+			maior = *p;
 		if (*p < menor)
-		    menor = *p;
+			menor = *p;
 		p++;
 	}
 }
 
-
-int main ()
+int main()
 {
-	list<int>fila;//declarando a fila
-	
-	int x;//variavel pra leitura
+	list<int> fila; // declarando a fila
+
+	int x; // variavel pra leitura
 	int menor;
 	int maior;
-	
-	cin >> x; //lendo x e inserindo na lista
+
+	cin >> x; // lendo x e inserindo na lista
 	do
 	{
 		fila.push_back(x);
 		cin >> x;
-    }
-	}while(x !=0);
-	
+	} while (x != 0);
+
 	// Descobrindo maior e menor elementos
 	maiorMenor(fila, menor, maior);
-	
-	// Mostrando maior e menor elemento 
-	cout << "menor: " << menor << endl; 
+
+	// Mostrando maior e menor elemento
+	cout << "menor: " << menor << endl;
 	cout << "maior: " << maior << endl;
-	
-	//liberando a memoria alocada pela lista ligada
+
+	// liberando a memoria alocada pela lista ligada
 	while (!fila.empty())
-		lista.pop_front();
-		
+
+		fila.pop_front();
+
 	return 0;
 }
