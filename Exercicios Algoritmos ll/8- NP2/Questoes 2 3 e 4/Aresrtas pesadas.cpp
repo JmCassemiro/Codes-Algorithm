3710516c720494128ee36e5807620d46bfbc4072
João Marcos Cassemiro <joaomarcos.jm@ges.inatel.br>
2025-02-06 12:47:31 -0300

    Editando Arquivos

--- Exercicios Algoritmos ll/8- NP2/Questoes 2 3 e 4/Aresrtas pesadas.cpp
+++ Exercicios Algoritmos ll/8- NP2/Questoes 2 3 e 4/Aresrtas pesadas.cpp
@@ -5,44 +5,43 @@ using namespace std;
 
 struct no
 {
-	int v; // variavel destino para struct no.
+	int v;	  // variavel destino para struct no
 	int peso; // variavel peso para struct no.
 };
 
-void cria_aresta(list<no>adj[], int u, int v, int peso, int orientado, int nVertices)
+void cria_aresta(list<no> adj[], int u, int v, int peso, int orientado, int nVertices)
 {
-	no aux; // declaração do struct no como variavel aux.
+	no aux; // declaraï¿½ï¿½o do struct no como variavel aux.
 
 	aux.v = v;
 	aux.peso = peso;
 	adj[u].push_back(aux);
-	if(orientado == 0)
+	if (orientado == 0)
 	{
 		aux.v = u;
 		adj[v].push_back(aux);
-
 	}
 }
 
-int arestasPesadas(list<no>adj[], int nVertices, int s, int limiar)
+int arestasPesadas(list<no> adj[], int nVertices, int s, int limiar)
 {
 	list<no>::iterator m;
 	list<int> Q;
-	no aux; //declaração da struct como variavel aux;
-	int u; // variavel origem
+	no aux;	   // declaraï¿½ï¿½o da struct como variavel aux;
+	int u;	   // variavel origem
 	int p[10]; // variavel pais
 	char state[10];
-	int v; // variavel destino;
-	int cont = 0; // variavel contador para contar quantas variaveis peso são maiores que limiar.
+	int v;		  // variavel destino;
+	int cont = 0; // variavel contador para contar quantas variaveis peso sï¿½o maiores que limiar.
 
 	for (u = 0; u < nVertices; u++)
 	{
 		state[u] = 'u'; // nao descoberto
-		p[u] = -1; // sem país
+		p[u] = -1;		// sem paï¿½s
 	}
 
 	state[s] = 'd'; // descoberto
-	p[s] = -1; // sem país
+	p[s] = -1;		// sem paï¿½s
 	Q.push_back(s);
 
 	while (!Q.empty())
@@ -50,15 +49,16 @@ int arestasPesadas(list<no>adj[], int nVertices, int s, int limiar)
 		u = *Q.begin();
 		Q.pop_front();
 
-		for (m = adj[u].begin() ; m != adj[u].end() ; m++)
+		for (m = adj[u].begin(); m != adj[u].end(); m++)
 		{
-			v = m -> v;
-			aux.peso = m -> peso;
+			v = m->v;
+			aux.peso = m->peso;
 
 			if (state[v] == 'u')
 			{
 				state[v] = 'd';
 				p[v] = u;
+
 				Q.push_back(v);
 			}
 
@@ -68,33 +68,32 @@ int arestasPesadas(list<no>adj[], int nVertices, int s, int limiar)
 			}
 		}
 
-		state[u] = 'p'; //processado
+		state[u] = 'p'; // processado
 	}
 
 	return cont;
 }
 
-int main ()
+int main()
 {
 	list<no>::iterator q;
 	list<no> adj[10];
-	int u; //variavel origem
+	int u; // variavel origem
 	no aux;
 	int nVertices;
-	int v; //variavel destino;
+	int v; // variavel destino;
 	int peso;
-	int s; //variavel Start;
-	int orientado = 1; // declaração de grafo como orientado;
-	int limiar; //variavel utilizada para achar peso maior do que a mesma.
-	int c; //int que tera o valor do retorno da função aresta pesada.
+	int s;			   // variavel Start;
+	int orientado = 1; // declaraï¿½ï¿½o de grafo como orientado;
+	int limiar;		   // variavel utilizada para achar peso maior do que a mesma.
+	int c;			   // int que tera o valor do retorno da funï¿½ï¿½o aresta pesada.
 
-	cin >> nVertices >> s; //entrada com nVertices e variavel start.
-
+	cin >> nVertices >> s; // entrada com nVertices e variavel start.
 
 	// Lnedo arestas
-	while ( u == -1 && v == -1 && peso == -1)
+	while (u == -1 && v == -1 && peso == -1)
 	{
-		cria_aresta(adj, u, v, peso, orientado, nVertices); //função cria aresta.
+		cria_aresta(adj, u, v, peso, orientado, nVertices); // funï¿½ï¿½o cria aresta.
 		cin >> u >> v >> peso;
 	}
 
